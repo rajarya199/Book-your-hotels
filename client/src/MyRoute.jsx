9b182543d05bc1a8249d763771f12cd2e31b2e18
rajarya199 <aryalrajan078@gmail.com>
2024-04-22 13:33:46 +0545

    register page

--- client/src/MyRoute.jsx
+++ client/src/MyRoute.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Layout from "./components/Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import Register from "./pages/Register";
 
 const MyRoute = () => {
   return (
@@ -12,6 +13,7 @@ const MyRoute = () => {
             <Route path='/' element={ <Layout/>}>
                 <Route index element={<IndexPage/>}/>
                 <Route path='/login' element={<LoginPage/> }/>
+                <Route path='/register' element={<Register/>}/>
                 </Route>
         </Routes>
     </Router>
